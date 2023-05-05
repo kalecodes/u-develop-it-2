5e53f3fe3e42536357f71e0001b2f63a4565d80d
kalen510 <kalen.wiley@gmail.com>
2023-05-05 18:53:23 -0400

    create express server

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// Express middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// Routes
+
+
+// Default request for any other request (Not Found)
+app.use((req, res) => {
+    res.status(404).end();
+});
+
+// connection function (lsiten for requests)
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
