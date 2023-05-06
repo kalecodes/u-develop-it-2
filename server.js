792d4a8773a7a53c676b4e892a74693985fb2843
kalen510 <kalen.wiley@gmail.com>
2023-05-05 20:03:49 -0400

    server issues

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const mysql = require('mysql2');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -7,15 +8,29 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+// Connect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: 'root',
+        password: '',
+        database: 'election'
+    }, 
+    console.log('Connected to the election database.')
+);
+
+db.promise().query(`SELECT * FROM candidates`)
+    .then((err, rows) => {
+        console.log(rows);
+    });
+
 // Routes
-
-
 // Default request for any other request (Not Found)
 app.use((req, res) => {
     res.status(404).end();
 });
 
-// connection function (lsiten for requests)
+// connection function (lsiten for requests) 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
